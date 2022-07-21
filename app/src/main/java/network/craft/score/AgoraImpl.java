e719f20b503c20996322d0261fce1fced9aef54c
Jaechang Namgoong <jnamgoong@iconloop.com>
2022-07-21 21:26:42 +0900

    Implement basic setter and getter methods

--- app/src/main/java/network/craft/score/AgoraImpl.java
+++ app/src/main/java/network/craft/score/AgoraImpl.java
@@ -17,29 +17,94 @@
 package network.craft.score;
 
 import score.Address;
+import score.Context;
+import score.VarDB;
 import score.annotation.EventLog;
 import score.annotation.External;
+import score.annotation.Optional;
 
 import java.math.BigInteger;
 import java.util.Map;
 
 public class AgoraImpl implements AgoraGov {
-    public AgoraImpl() {
+    private static final String IRC2 = "irc-2";
+    private static final String IRC31 = "irc-31";
+
+    private final VarDB<Address> govToken = Context.newVarDB("gov_token", Address.class);
+    private final VarDB<String> tokenType = Context.newVarDB("token_type", String.class);
+    private final VarDB<BigInteger> tokenId = Context.newVarDB("token_id", BigInteger.class);
+    private final VarDB<BigInteger> minimumThreshold = Context.newVarDB("minimum_threshold", BigInteger.class);
+
+    private void checkCallerOrThrow(Address caller, String errMsg) {
+        Context.require(Context.getCaller().equals(caller), errMsg);
+    }
+
+    private void onlyOwner() {
+        checkCallerOrThrow(Context.getOwner(), "OnlyOwner");
+    }
+
+    @External(readonly=true)
+    public Map<String, Object> governanceTokenInfo() {
+        var type = tokenType();
+        if (type == null) {
+            return Map.of();
+        }
+        if (IRC2.equals(type)) {
+            return Map.of(
+                    "_address", govToken(),
+                    "_type", type);
+        } else {
+            return Map.of(
+                    "_address", govToken(),
+                    "_type", type,
+                    "_id", tokenId());
+        }
+    }
+
+    private Address govToken() {
+        return govToken.get();
+    }
+
+    private String tokenType() {
+        return tokenType.get();
+    }
+
+    private BigInteger tokenId() {
+        return tokenId.getOrDefault(BigInteger.ZERO);
     }
 
     @External
-    public void setGovernanceToken(Address _address, String _type, BigInteger _id) {
+    public void setGovernanceToken(Address _address, String _type, @Optional BigInteger _id) {
+        onlyOwner();
+        var type = _type.toLowerCase();
+        switch (type) {
+            case IRC2:
+            case IRC31:
+                tokenType.set(type);
+                break;
+            default:
+                Context.revert("InvalidTokenType");
+        }
+        govToken.set(_address);
+        if (IRC31.equals(type)) {
+            tokenId.set(_id);
+        }
+    }
 
+    @External(readonly=true)
+    public BigInteger minimumThreshold() {
+        return minimumThreshold.getOrDefault(BigInteger.ZERO);
     }
 
     @External
     public void setMinimumThreshold(BigInteger _amount) {
-
+        onlyOwner();
+        Context.require(_amount.signum() > 0, "Minimum threshold must be positive");
+        minimumThreshold.set(_amount);
     }
 
     @External
     public void submitProposal(BigInteger _endTime, String _ipfsHash) {
-
     }
 
     @External
